a78e54ccc46726dc4f52064bcbe7852f1e33034d
cleitonsilva14 <cleiton18ads@gmail.com>
2026-01-30 19:41:16 -0300

    Adicionando funcionalidade para adicionar Tweet

---
+++ src/main/java/br/com/springsecurity6jwtoauth2/controller/TweetController.java
@@ -0,0 +1,39 @@
+package br.com.springsecurity6jwtoauth2.controller;
+
+import br.com.springsecurity6jwtoauth2.domain.Tweet;
+import br.com.springsecurity6jwtoauth2.dto.CreateTweetDto;
+import br.com.springsecurity6jwtoauth2.repository.TweetRepository;
+import br.com.springsecurity6jwtoauth2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class TweetController {
+
+    private final TweetRepository tweetRepository;
+    private final UserRepository userRepository;
+
+    @PostMapping("/tweet")
+    public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDto dto,
+                                            JwtAuthenticationToken token) {
+
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+
+        var tweet = new Tweet();
+        tweet.setUser(user.get());
+        tweet.setContent(dto.content());
+
+        tweetRepository.save(tweet);
+
+        return ResponseEntity.ok().build();
+    }
+
+
+}
