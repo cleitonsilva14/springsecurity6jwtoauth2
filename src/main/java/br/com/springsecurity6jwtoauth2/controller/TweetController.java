7659de653b13f019169e5f7bfd4a703dfae4e333
cleitonsilva14 <cleiton18ads@gmail.com>
2026-02-03 11:08:21 -0300

    Adicionando funcionalidade de deletar Tweet

--- src/main/java/br/com/springsecurity6jwtoauth2/controller/TweetController.java
+++ src/main/java/br/com/springsecurity6jwtoauth2/controller/TweetController.java
@@ -1,15 +1,17 @@
 package br.com.springsecurity6jwtoauth2.controller;
 
+import br.com.springsecurity6jwtoauth2.domain.Role;
 import br.com.springsecurity6jwtoauth2.domain.Tweet;
 import br.com.springsecurity6jwtoauth2.dto.CreateTweetDto;
 import br.com.springsecurity6jwtoauth2.repository.TweetRepository;
 import br.com.springsecurity6jwtoauth2.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -31,6 +33,35 @@ public class TweetController {
         tweet.setContent(dto.content());
 
         tweetRepository.save(tweet);
+
+        return ResponseEntity
+                .ok()
+                .build();
+    }
+
+    @DeleteMapping("/tweet/{tweetId}")
+    public ResponseEntity<Void> deleteTweet(@PathVariable("tweetId") Long tweetId, JwtAuthenticationToken token) {
+
+        var  user = userRepository.findById(UUID.fromString(token.getName()));
+
+
+
+
+        var tweet = tweetRepository.findById(tweetId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get()
+                .getRoles()
+                .stream()
+                .anyMatch(role -> role.getName().equals(Role.Values.ADMIN.name()));
+
+        if( isAdmin || tweet.getUser().getUserUUID().equals(UUID.fromString(token.getName()))){
+            tweetRepository.deleteById(tweetId);
+        }else {
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .build();
+        }
 
         return ResponseEntity
                 .ok()
